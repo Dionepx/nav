bbe04b6c5dc3d5fe423abfe1867b2e62f7df4e11
Dione V. Silva <135567312+Dionepx@users.noreply.github.com>
2024-05-21 23:56:43 -0300

    Add files via upload

---
+++ nav/scripts.js
@@ -0,0 +1,8 @@
+<nav>
+  <ul>
+    <li><a href="#home">Home</a></li>
+    <li><a href="#about">Sobre</a></li>
+    <li><a href="#services">Serviços</a></li>
+    <li><a href="#contact">Contato</a></li>
+  </ul>
+</nav>
